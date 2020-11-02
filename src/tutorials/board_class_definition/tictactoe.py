3d2006f57acedb65ffaf4072aaa21296919d5d13
Maksim Korzh <freesoft.for.people@gmail.com>
2020-11-02 19:31:04 +0200

    added board class definition tutorial

---
+++ src/tutorials/board_class_definition/tictactoe.py
@@ -0,0 +1,99 @@
+#
+# AI that learns to play Tic Tac Toe using
+#        reinforcement learning
+#             (MCTS + NN)
+#
+
+# packages
+from copy import deepcopy
+
+# Tic Tac Toe board class
+class Board():
+    # create constructor (init board class instance)
+    def __init__(self, board=None):
+        # define players
+        self.player_1 = 'x'
+        self.player_2 = 'o'
+        self.empty_square = '.'
+        
+        # define board position
+        self.position = {}
+        
+        # init (reset) board
+        self.init_board()
+        
+        # create a copy of a previous board state if available
+        if board is not None:
+            self.__dict__ = deepcopy(board.__dict__)
+    
+    # init (reset) board
+    def init_board(self):
+        # loop over board rows
+        for row in range(3):
+            # loop over board columns
+            for col in range(3):
+                # set every board square to empty square
+                self.position[row, col] = self.empty_square
+    
+    # make move
+    def make_move(self, row, col):
+        # create new board instance
+        board = Board()
+        
+        # make move
+        board.position[row, col] = self.player_1
+        
+        # swap players
+        (board.player_1, board.player_2) = (board.player_2, board.player_1)
+    
+        # return new board state
+        return board
+        
+    # print board state
+    def __str__(self):
+        # define board string representation
+        board_string = ''
+        
+        # loop over board rows
+        for row in range(3):
+            # loop over board columns
+            for col in range(3):
+                board_string += ' %s' % self.position[row, col]
+            
+            # print new line every row
+            board_string += '\n'
+        
+        # prepend side to move
+        if self.player_1 == 'x':
+            board_string = '\n--------------\n "x" to move:\n--------------\n\n' + board_string
+        
+        elif self.player_1 == 'o':
+            board_string = '\n--------------\n "o" to move:\n--------------\n\n' + board_string
+                        
+        # return board string
+        return board_string
+
+# main driver
+if __name__ == '__main__':
+    # create board instance
+    board = Board()
+
+    # print initial board state
+    print(board)
+    print(board.__dict__)  
+    
+    # make move on board
+    board = board.make_move(0, 0)
+    
+    # create opponent's board instance
+    board_1 = Board(board)
+    print(board_1)
+    print(board_1.__dict__)
+    
+
+    
+    
+    
+    
+    
+    
