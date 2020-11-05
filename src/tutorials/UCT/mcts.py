f8068a30958500080d4e1d2e27fab880202c7367
Maksim Korzh <freesoft.for.people@gmail.com>
2020-11-05 18:26:06 +0200

    added UCT tutorial

---
+++ src/tutorials/UCT/mcts.py
@@ -0,0 +1,131 @@
+#
+# MCTS algorithm implementation
+#
+
+# packages
+import math
+import random
+
+# tree node class definition
+class TreeNode():
+    # class constructor (create tree node class instance)
+    def __init__(self, board, parent):
+        # init associated board state
+        self.board = board
+        
+        # init is node terminal flag
+        if self.board.is_win() or self.board.is_draw():
+            # we have a terminal node
+            self.is_terminal = True
+        
+        # otherwise
+        else:
+            # we have a non-terminal node
+            self.is_terminal = False
+        
+        # init is fully expanded flag
+        self.is_fully_expanded = self.is_terminal
+        
+        # init parent node if available
+        self.parent = parent
+        
+        # init the number of node visits
+        self.visits = 0
+        
+        # init the total score of the node
+        self.score = 0
+        
+        # init current node's children
+        self.children = {}
+
+# MCTS class definition
+class MCTS():
+    # search for the best move in the current position
+    def search(self, initial_state):
+        # create root node
+        self.root = TreeNode(initial_state, None)
+
+        # walk through 1000 iterations
+        for iteration in range(1000):
+            # select a node (selection phase)
+            node = self.select(self.root)
+            
+            # scrore current node (simulation phase)
+            score = self.rollout(node.board)
+            
+            # backpropagate results
+            self.backpropagate(node, score)
+        
+        # pick up the best move in the current position
+        try:
+            return get_best_move(self.root, 0)
+        
+        except:
+            pass
+    
+    # select most promising node
+    def select(self, node):
+        pass
+    
+    # simulate the game via making random moves until reach end of the game
+    def rollout(self, board):
+        pass
+    
+    # backpropagate the number of visits and score up to the root node
+    def backpropagate(self, node, score):
+        pass
+    
+    # select the best node basing on UCB1 formula
+    def get_best_move(self, node, exploration_constant):
+        # define best score & best moves
+        best_score = float('-inf')
+        best_moves = []
+        
+        # loop over child nodes
+        for child_node in node.children.values():
+            # define current player
+            if child_node.board.player_2 == 'x': current_player = 1
+            elif child_node.board.player_2 == 'o': current_player = -1
+            
+            # get move score using UCT formula
+            move_score = current_player * child_node.score / child_node.visits + exploration_constant * math.sqrt(math.log(node.visits / child_node.visits))                                        
+            print('move_score:', move_score)
+            # better move has been found
+            if move_score > best_score:
+                best_score = move_score
+                best_moves = [child_node]
+            
+            # found as good move as already available
+            elif move_score == best_score:
+                best_moves.append(child_node)
+            
+        # return one of the best moves randomly
+        return random.choice(best_moves)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
